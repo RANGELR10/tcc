d070f24ba140404c6a372f912da60f2f1ac4505a
RANGELR10 <164943594+RANGELR10@users.noreply.github.com>
2024-03-25 17:54:01 -0300

    Add files via upload

---
+++ models.js
@@ -0,0 +1,11 @@
+let modelsJson = [
+    {id:1, name:' barcelona #33 ', img:'TIME1.webp', price:[189.90, 189.90, 189.90], sizes:['1/72', '1/48', '1/32'], description:''},
+    {id:2, name:' Manchester-city #22', img:'TIME2.webp', price:[189.90, 189.90, 189.90], sizes:['1/72', '1/48', '1/32'], description:''},
+    {id:3, name:' Real Madrid #44', img:'TIME3.jpg', price:[189.90, 189.90, 189.900], sizes:['1/72', '1/48', '1/32'], description:''},
+    {id:4, name:' Real Madrid #5', img:'TIME4.jpg', price:[189.90, 189.90, 189.90], sizes:['1/72', '1/48', '1/32'], description:''},
+    {id:5, name:' Barcelona #20', img:'TIME5.webp', price:[189.90, 189.90, 189.90], sizes:['1/72', '1/48', '1/32'], description:''},
+    {id:6, name:' Brasil 2024 #13', img:'TIME6.webp', price:[189.90, 189.90, 189.90], sizes:['1/72', '1/48', '1/32'], description:''},
+    {id:7, name:' Flamengo 2024 #77', img:'TIME7.webp', price:[189.90, 189.90, 189.90], sizes:['1/72', '1/48', '1/32'], description:''},
+    {id:8, name:' INTERNACIONAL #88', img:'TIME8.jpg', price:[189.90, 189.90, 189.90], sizes:['1/72', '1/48', '1/32'], description:''}
+    
+];
